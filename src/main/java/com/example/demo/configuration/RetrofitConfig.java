67226aa67c73ab0eb62857a1159b00c6977bb74c
Rajat Sharma <rajatxd007@gmail.com>
2025-08-18 12:01:28 +0530

    Added code to fetch fakestore varaible from env variables

--- src/main/java/com/example/demo/configuration/RetrofitConfig.java
+++ src/main/java/com/example/demo/configuration/RetrofitConfig.java
@@ -2,18 +2,23 @@ package com.example.demo.configuration;
 
 
 import com.example.demo.gateway.Api.FakeStoreCategoryApi;
+import lombok.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 @Configuration
 public class RetrofitConfig {
+    @Autowired
+    private Environment env;
 
     @Bean
     public Retrofit retrofit(){
         return new Retrofit.Builder()
-                .baseUrl("https://fakestoreapi.in/api/")
+                .baseUrl(env.getProperty("fakestore_url"))
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
     }
